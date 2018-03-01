4aa3ffaeed880db25a149251ebf27003df5f62f1
Kishan Shah <kshah1125@gmail.com>
2018-03-02 00:11:46 +0530

    vuex and dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,52 +1,29 @@
-<template style="height:100%;">
-  <v-container style="height:100%;" grid-list-md>
-      <v-layout row wrap style="height:100%;">
-          <!-- <v-flex xs-6 dark color="primary"> -->
-              <v-card id="suppliers" width="30%" height="30%">
-                  <v-card-text width="30%">
-                      <h1>suppliers</h1>
-                  </v-card-text>
-              </v-card>
-          <!-- </v-flex> -->
-              
-              <v-card id="Customers" width="30%" height="30%">
-                  <v-card-text>
-                      <h1>Customers</h1>
-                  </v-card-text>
-              </v-card>
-              <v-card id="Users" width="30%" height="30%">
-                    <v-card-text>
-                      <h1>Users</h1>
-                  </v-card-text>
-              </v-card>
-              <v-card id="Expense">
-
-              </v-card>
-              <v-card id="Products">
-
-              </v-card>
-              <v-card id="Purchase">
-
-              </v-card>
-              <v-card id="Sales">
-
-              </v-card>
-              <v-card id="History">
-
-              </v-card>
-              <v-card id="Pending Invoice">
-
-              </v-card>
-              <v-card id="Damage Issue">
-
-              </v-card>
-              <v-card id="Restore">
-
-              </v-card>
-              <v-card id="Settings">
-
-              </v-card>
-          </v-flex>
-      </v-layout>
+<template>
+  <v-container fluid grid-list-md text-xs-center mt-5>
+    <v-layout row wrap mt-5>
+      
+      <v-flex d-flex xs3>
+        <v-card dark color="primary" height="200px">
+          <v-card-text class="px-0">3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs3>
+        <v-card dark color="secondary" height="200px">
+          <v-card-text class="px-0">3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-layout row wrap>
+      <v-flex d-flex xs3>
+        <v-card dark color="primary" height="200px">
+          <v-card-text class="px-0">3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs3>
+        <v-card dark color="secondary" height="200px">
+          <v-card-text class="px-1">3</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    </v-layout>
   </v-container>
 </template>
