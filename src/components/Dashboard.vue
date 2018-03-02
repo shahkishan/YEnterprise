e5fa7497de262d32794c5802c2576ee4f7970b01
Kishan Shah <kshah1125@gmail.com>
2018-03-02 14:47:37 +0530

    dashboard cards alignment

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,29 +1,77 @@
 <template>
   <v-container fluid grid-list-md text-xs-center mt-5>
-    <v-layout row wrap mt-5>
+    <v-layout v-bind="binding">
+      <!-- <> -->
+      <v-flex d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="primary" height="200px" width="100%">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="secondary" height="200px" width="100%">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
+      <!-- </> -->
+      <!-- <> -->
+      <v-flex d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="primary" height="200px" width="100%">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="secondary" height="200px">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
       
-      <v-flex d-flex xs3>
-        <v-card dark color="primary" height="200px">
-          <v-card-text class="px-0">3</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex d-flex xs3>
-        <v-card dark color="secondary" height="200px">
-          <v-card-text class="px-0">3</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-layout row wrap>
-      <v-flex d-flex xs3>
-        <v-card dark color="primary" height="200px">
-          <v-card-text class="px-0">3</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex d-flex xs3>
-        <v-card dark color="secondary" height="200px">
-          <v-card-text class="px-1">3</v-card-text>
-        </v-card>
-      </v-flex>
+      <!-- </> -->
+      
     </v-layout>
+
+    <v-layout v-bind="binding">
+      <!-- <> -->
+      <v-flex d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="primary" height="200px" width="100%" raised  hover="">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex  d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="secondary" height="200px" width="100%">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
+      <!-- </> -->
+      <!-- <> -->
+      <v-flex d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="primary" height="200px" width="100%">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex d-flex xs12 md6 sm8 offset-md0 offset-sm2>
+        <v-card dark color="secondary" height="200px">
+          <v-card-text>3</v-card-text>
+        </v-card>
+      </v-flex>
+      
+      <!-- </> -->
+      
     </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {
+    computed: {
+      binding () {
+        const binding = {}
+
+        if (this.$vuetify.breakpoint.mdAndUp) binding.row = true
+        else
+          binding.column=true
+
+        return binding
+      }
+    }
+  }
+</script>
